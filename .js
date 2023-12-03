3c56f0105cdbc3e505c95c6427f05338cda16419
sept.sept <sept.kobokankamu@gmail.com>
2023-12-03 11:27:37 +0700

    initial commit

--- .js
+++ .js
@@ -1,8 +1,8 @@
-const wrapper= document.querySelector(".wrapper");
-const question= document.querySelector(".question");
-const gift= document.querySelector(".gift");
-const yesBtn= document.querySelector(".yes-btn");
-const noBtn= document.querySelector(".no-btn");
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gift = document.querySelector(".gift");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
 
 yesBtn,addEventListener("click", () => {
     question.innerHTML = "Aaaaa, I like you too";
@@ -10,13 +10,13 @@ yesBtn,addEventListener("click", () => {
 });
 
 noBtn.addEventListener("mouscover", () => {
-    const noBtnRect= noBtn.getBoundingClientRect();
-    const maxX= window.innerWidth - noBtnRect.width;
-    const maxY= window.innerHeight - noBtnRect.height;
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
 
-    const randomX= Math.floor(Math.random() * maxX)
-    const randomY= Math.floor(Math.random() * maxY)
+    const randomX = Math.floor(Math.random() * maxX)
+    const randomY = Math.floor(Math.random() * maxY)
 
-    noBtn.style.left= randomX + "px";
-    noBtn.style.top= randomY + "px";
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
 })
